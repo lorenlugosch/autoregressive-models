334c8e42e16222a2ef4b4bdafea55fe36a87668f
Loren Lugosch <lugosch@orthanc.ece.mcgill.ca>
2020-01-08 11:49:57 -0500

    autoregressive model seems to work, uploading code and model

---
+++ data.py
@@ -0,0 +1,114 @@
+import torch
+import torch.utils.data
+import torchaudio
+import os
+import soundfile as sf
+import numpy as np
+import configparser
+import multiprocessing
+import json
+import pandas as pd
+from subprocess import call
+
+class Config:
+	def __init__(self):
+		pass
+
+def read_config(config_file):
+	config = Config()
+	parser = configparser.ConfigParser()
+	parser.read(config_file)
+
+	#[experiment]
+	config.seed=int(parser.get("experiment", "seed"))
+	config.folder=parser.get("experiment", "folder")
+
+	# Make a folder containing experiment information
+	if not os.path.isdir(config.folder):
+		os.mkdir(config.folder)
+		os.mkdir(os.path.join(config.folder, "training"))
+	call("cp " + config_file + " " + os.path.join(config.folder, "experiment.cfg"), shell=True)
+
+	#[model]
+	config.num_layers=int(parser.get("model", "num_layers"))
+	config.num_hidden=int(parser.get("model", "num_hidden"))
+	config.num_mel_bins=int(parser.get("model", "num_mel_bins"))
+
+	#[training]
+	config.base_path=parser.get("training", "base_path")
+	config.lr=float(parser.get("training", "lr"))
+	config.lr_period=int(parser.get("training", "lr_period"))
+	config.gamma=float(parser.get("training", "gamma"))
+	config.batch_size=int(parser.get("training", "batch_size"))
+	config.num_epochs=int(parser.get("training", "num_epochs"))
+
+	return config
+
+def get_ASR_datasets(config):
+	"""
+	config: Config object (contains info about model and training)
+	"""
+	base_path = config.base_path
+
+	# Get dfs
+	train_df = pd.read_csv(os.path.join(base_path, "train-clean-360.csv")) #"train_data.csv"))
+	valid_df = pd.read_csv(os.path.join(base_path, "valid_data.csv"))
+	test_df = pd.read_csv(os.path.join(base_path, "test_data.csv"))
+
+	# Create dataset objects
+	train_dataset = ASRDataset(train_df, config)
+	valid_dataset = ASRDataset(valid_df, config)
+	test_dataset = ASRDataset(test_df, config)
+
+	return train_dataset, valid_dataset, test_dataset
+
+class ASRDataset(torch.utils.data.Dataset):
+	def __init__(self, df, config):
+		"""
+		df: dataframe of wav file paths and transcripts
+		config: Config object (contains info about model and training)
+		"""
+		# dataframe with wav file paths, transcripts
+		self.df = df
+		self.base_path = config.base_path
+		self.loader = torch.utils.data.DataLoader(self, batch_size=config.batch_size, num_workers=multiprocessing.cpu_count(), shuffle=True, collate_fn=CollateWavsASR())
+
+	def __len__(self):
+		return len(self.df)
+
+	def __getitem__(self, idx):
+		x, fs = sf.read(os.path.join(self.base_path, self.df.path[idx]))
+		return (x, idx)
+
+class CollateWavsASR:
+	def __init__(self):
+		self.max_length = 500000
+
+	def __call__(self, batch):
+		"""
+		batch: list of tuples (input wav, output labels)
+
+		Returns a minibatch of wavs and labels as Tensors.
+		"""
+		x = []; idxs = []
+		batch_size = len(batch)
+		for index in range(batch_size):
+			x_,idx = batch[index]
+
+			# throw away large audios
+			if len(x_) < self.max_length:
+				x.append(torch.tensor(x_).float())
+				idxs.append(idx)
+
+		batch_size = len(idxs) # in case we threw some away
+
+		# pad all sequences to have same length
+		T = [len(x_) for x_ in x]
+		T_max = max(T)
+		for index in range(batch_size):
+			x_pad_length = (T_max - len(x[index]))
+			x[index] = torch.nn.functional.pad(x[index], (0,x_pad_length))
+
+		x = torch.stack(x)
+
+		return (x,T,idxs)
---
+++ models.py
@@ -0,0 +1,148 @@
+import torch
+import torchaudio
+import numpy as np
+import sys
+import os
+import matplotlib.pyplot as plt
+
+class AutoregressiveModel(torch.nn.Module):
+	def __init__(self, config):
+		super(AutoregressiveModel, self).__init__()
+		self.compute_fbank = ComputeFBANK(config)
+		self.encoder = Encoder(config)
+		self.regressor = torch.nn.Linear(self.encoder.out_dim, config.num_mel_bins)
+
+	def load_pretrained(self, model_path=None):
+		if model_path == None:
+			model_path = os.path.join(self.checkpoint_path, "model_state.pth")
+		device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+		self.load_state_dict(torch.load(model_path, map_location=device))
+
+	def forward(self, x, T):
+		# move inputs to GPU
+		if next(self.parameters()).is_cuda:
+			x = x.cuda()
+
+		fbank = self.compute_fbank((x,T))
+		encoder_out = self.encoder.forward(fbank, T)
+		x_predicted = self.regressor.forward(encoder_out)
+		diff = x_predicted[:,:-1,:] - fbank
+
+		return encoder_out, x_predicted, diff
+
+class Encoder(torch.nn.Module):
+	def __init__(self, config):
+		super(Encoder, self).__init__()
+		self.layers = []
+
+		# input is FBANK
+		layer = FBANKNormalizer(config)
+		self.layers.append(layer)
+		out_dim = config.num_mel_bins
+
+		# pad to be causal
+		layer = CausalPad()
+		self.layers.append(layer)
+
+		for idx in range(config.num_layers):
+			# recurrent
+			layer = torch.nn.GRU(input_size=out_dim, hidden_size=config.num_hidden, batch_first=True, bidirectional=False)
+			self.layers.append(layer)
+			out_dim = config.num_hidden
+
+			# grab hidden states for each timestep
+			layer = RNNOutputSelect()
+			self.layers.append(layer)
+
+			# dropout
+			layer = torch.nn.Dropout(p=0.5)
+			self.layers.append(layer)
+
+			# fully-connected
+			layer = torch.nn.Linear(out_dim, config.num_hidden)
+			out_dim = config.num_hidden
+			self.layers.append(layer)
+			layer = torch.nn.LeakyReLU(0.125)
+			self.layers.append(layer)
+
+		self.layers = torch.nn.ModuleList(self.layers)
+		self.out_dim = out_dim
+
+	def forward(self, x, T):
+		out = x
+		for layer in self.layers:
+			#print(out.shape, out.min(), out.max())
+			out = layer(out)
+
+		return out
+
+
+class RNNOutputSelect(torch.nn.Module):
+	def __init__(self):
+		super(RNNOutputSelect, self).__init__()
+
+	def forward(self, input):
+		return input[0]
+
+class NCL2NLC(torch.nn.Module):
+	def __init__(self):
+		super(NCL2NLC, self).__init__()
+
+	def forward(self, input):
+		"""
+		input : Tensor of shape (batch size, T, Cin)
+		Outputs a Tensor of shape (batch size, Cin, T).
+		"""
+
+		return input.transpose(1,2)
+
+class CausalPad(torch.nn.Module):
+	def __init__(self):
+		super(CausalPad, self).__init__()
+
+	def forward(self, input):
+		return torch.nn.functional.pad(input, (0,0,1,0))
+
+class FBANKNormalizer(torch.nn.Module):
+	def __init__(self, config):
+		super(FBANKNormalizer,self).__init__()
+		self.num_mel_bins = config.num_mel_bins
+		self.weight = torch.nn.Parameter(torch.tensor([1/10] * self.num_mel_bins))
+		self.bias = torch.nn.Parameter(torch.tensor([0.] * self.num_mel_bins))
+
+	def forward(self, fbank):
+		out = fbank + self.bias.unsqueeze(0)
+		out = out * self.weight.unsqueeze(0)
+		return out
+
+class ComputeFBANK(torch.nn.Module):
+	def __init__(self, config):
+		super(ComputeFBANK,self).__init__()
+		self.num_mel_bins = config.num_mel_bins
+		self.fbank_params = {
+                    "channel": 0,
+                    "dither": 0.0,
+                    "window_type": "hanning",
+                    "num_mel_bins":self.num_mel_bins,
+                    "remove_dc_offset": False,
+                    "round_to_power_of_two": False,
+                    "sample_frequency":16000.0,
+                }
+
+	def forward(self, input):
+		"""
+		input : (x,T)
+		x : waveforms
+		T : durations
+		returns : (normalized) FBANK feature vectors
+		"""
+		fbanks = []
+		x,T = input
+		batch_size = len(x)
+		for idx in range(batch_size):
+			fbank_ = torchaudio.compliance.kaldi.fbank(x[idx].unsqueeze(0), **self.fbank_params)
+			fbanks.append(fbank_)
+
+		fbank = torch.stack(fbanks)
+		return fbank
+
